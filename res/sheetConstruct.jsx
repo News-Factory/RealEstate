8ebec8089b28f098adac51a139f233616ca68a0d
orenmenache <72181196+orenmenache@users.noreply.github.com>
2020-12-16 13:08:40 +0200

    sheetConstruct kickoff

--- res/sheetConstruct.jsx
+++ res/sheetConstruct.jsx
@@ -1,26 +1,54 @@
 //Here we'll be using specific labels to construct the google sheet
-function constructGS(x){
-    //var selectedLabels=[6,10]; //peach, purple
-    var symbols=x.sym;
-    var peachLabels=labels_getAllLayersWithLabel(x,3);
-    var first=peachLabels[0];
-    makeTitleAndType(symbols,first,6);
-    //var 
+function sc_constructGS(x){
+    var selectedLabels=[6,10,12,13]; //peach, purple, brown, fuchsia
+    var labelTypes=sc_defineTypesByLabels(); //array of objects {colorName,type}
+    var resTitleRow=[]; //we're exporting a csv txt
+    var resLabelTypeRow=[]; //we'll be joining the arrayes to a text later
+
+    for (var i=0; i<selectedLabels.length; i++){
+        var labelNum=selectedLabels[i]; //6/10/12/13
+        var labelType=labelTypes[labelNum].type; //text,vid,pic,audio
+        var allLayers_inSelectedLabel=labels_getAllLayersWithLabel(x,labelNum);
+        for (var j=0; j<allLayers_inSelectedLabel.length; j++){
+            var layerName=allLayers_inSelectedLabel[j].name;
+            resTitleRow.push(layerName);
+            resLabelTypeRow.push(labelType);
+        }
+    }
+
+    var res=resTitleRow.join()+'\n';
+    res+=resLabelTypeRow.join();
+
+    var scriptFolderPath=File($.fileName).path;
+    //alert(scriptFolderPath);
+    
+    var outoutCSV=File(scriptFolderPath+'/test.csv');
+    outoutCSV.encoding ="utf-8";
+    outoutCSV.open("w");
+    outoutCSV.write(res);
+    outoutCSV.close();
+    
 }
 
-function makeTitleAndType(symbols,layer,label){
+function sc_makeTitleAndType(types,symbols,first,label){
+    //construct a single object containing layerName and type
     var layerName=layer.name;
-    var layerType=getLayerType(layer);
+    var type=
     alert(layerName);
     alert(layerType);
 }
 
-function sheetConstruct_defineTypesByLabels(){
-    //Labels peach, purple, brown and fuchsia will be used as follows:
-    var res={};
-    res.peach='text';
-    res.purple='vid';
-    res.brown='pic';
-    res.fuchsia='audio';
+function sc_defineTypesByLabels(){
+    //Labels peach, purple, brown and fuchsia will be used
+    var labels=[null,'red','yellow','aqua','pink','lavender','peach','seaFoam','blue','green','purple','orange','brown','fuchsia','cyan','sandstone','darkGreen'];
+    var res=[];
+    for (var i=1; i<labels.length; i++){
+        res.push({colorName:labels[i]});
+    }
+    res[6].type='text'; //peach
+    res[10].type='vid'; //purple
+    res[12].type='pic'; //brown
+    res[13].type='audio'; //fuchsia
+
     return res;
 }
