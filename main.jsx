bc188e01c738e51f9893e0104258fbbdaadfb36a
News-Factory <studio@motionhive.tv>
2021-03-01 17:08:28 +0200

    commit on 'Anglo-Saxon'

--- main.jsx
+++ main.jsx
@@ -20,7 +20,6 @@
 //Stage06 Set background music 
 
 {   
-
     //    HAVE A LOOK HERE!!!!!!
     
     //    HAVE A LOOK HERE!!!!!!!
@@ -28,13 +27,13 @@
     //    YOU MUST UNCOMMENT THE batchProcess() FUNCTION!!!!
 
 
-    // var mommyFolderPath='G:/My Drive/Real Estate Project/';
-    // var waitingFolder=new Folder(mommyFolderPath+'waiting2');  // the normal folder is "waiting"
-    // var wFiles=waitingFolder.getFiles();
-    // var txtFilePath=mommyFolderPath+waitingFolder.name+'/'+wFiles[0].name;
-    // var x=defineMainProjectItems(txtFilePath); 
-    // sc_constructGS(x);  // this function creates the google sheet thingy
-    batchProcess();
+    var mommyFolderPath='G:/My Drive/Real Estate Project/';
+    var waitingFolder=new Folder(mommyFolderPath+'waiting2');  // the normal folder is "waiting"
+    var wFiles=waitingFolder.getFiles();
+    var txtFilePath=mommyFolderPath+waitingFolder.name+'/'+wFiles[0].name;
+    var x=defineMainProjectItems(txtFilePath); 
+    sc_constructGS(x);  // this function creates the google sheet thingy
+    // batchProcess();
 }
 
 function batchProcess(){
