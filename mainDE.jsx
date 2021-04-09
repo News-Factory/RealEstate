0aec56a470f51766343ebf619c6416c5bcdd1f92
News-Factory <studio@motionhive.tv>
2021-04-09 13:31:02 +0300

    almost done

--- mainDE.jsx
+++ mainDE.jsx
@@ -92,7 +92,7 @@ function realEstateDE(x){
     setTheMusic(x);
 
     // Stage05
-    // slicer(x);
+    slicer(x);
 
     // setDurationForIntroComp(x);  // defines the lenght of the intro comp based on music
     // setDurationForOutroComp(x);  // defines the lenght of the outro comp based on the music
@@ -109,73 +109,10 @@ function realEstateDE(x){
 
 //Four functions to supplement the slicer
 function slicer(x){
-    var mainLayers = x.mainComp.layers;
-
-    // these variables are added because the function needs to set the length of the
-    // intro comp based on the music in the intro
-    var backgroundIntroSong = x.allLayers['0_Intro']['Intro Sound'];
-    var neededTime = backgroundIntroSong.source.duration;
-
-    if (x.photoComp) {//if naming was done correctly start
-
-        var pcLayer=x.allLayers['Photo Comp'].comp;
-        var photoLayers = pcLayer.layers;
-        var howMany_Pictures = photoLayers.length;
-        var introDuration = mainLayers[5].outPoint;
-        var gap = 2;
-        var locTestPhoto = getLoc_TestPhoto(x);
-
-        mainLayers[5].outPoint=neededTime;
-
-        ///// This if statement arranges the layers in [0_Main Comp] in different ways
-        ///// depending if the AE project has test pictures or not 23/12/2020
-        if(locTestPhoto){
-            var lastPic = x.allLayers['Photo Comp']['Room_Photo_'+(locTestPhoto)];         
-            mainLayers[6].outPoint = lastPic.inPoint + introDuration -gap;     
-            // alert(lastPic.inPoint);
-        } else {
-            var lastPic = x.allLayers['Photo Comp']['Room_Photo_10'];
-            mainLayers[6].outPoint = lastPic.inPoint + introDuration; 
-        } 
-
-        for (var i=5; i<8; i++){
-            var layer = mainLayers[i];
-            var nextLayer = mainLayers[i+1];
-            // alert(layer.name);
-            nextLayer.startTime=layer.outPoint-gap;    
-        } 
-        
-        // 20/01/2021  this part changes the length of the whole project that is gonna be exported 
-        var veryEnd=x.allLayers['0_Main Comp']['0_Outro'].outPoint;
-        var main= x.allLayers['0_Main Comp'].comp;
-        main.workAreaDuration = veryEnd;
-   
-    } else { //If naming hasn't been done correctly sound the alarm
-        alert("0_Main Comp or 1_Photos Comp were not found. Please make sure their labels are named correctly and try again.");
-    }
-}
-
-function getLoc_TestPhoto(x){//get the layer number where test photo is at
-
-    var pcLayer=x.allLayers['Photo Comp'].comp;
-    var photoLayers = pcLayer.layers;
-    var howMany_Pictures = photoLayers.length;
-
-    for (var j=1; j<=howMany_Pictures; j++){
-        var compName = "Room_Photo_"+j;
-        var comp = getByName(x.comps,compName);
-        // alert(compName);
-        var layerName = comp.layer(1);
-        if (layerName.name == "RoomP"+j){
-            var imageSourceName = layerName.source.name;  // check every layer for the image with the source
-            var imageSourceType = getFileType(imageSourceName);
-
-            if (imageSourceName=="test picture.jpg"){ //if the source is Test Photo we can then get the location
-                // alert('I found it!')
-                return j; 
-            }
-        }
-    } 
+    // 20/01/2021  this part changes the length of the whole project that is gonna be exported 
+    var main= x.allLayers['0_Main Comp'].comp;
+    var veryEnd=main.layers[3].outPoint;
+    main.workAreaDuration = veryEnd;
 }
 
 
@@ -187,13 +124,7 @@ function soundAndDetails(x){
     // alert(template);
 
     setLogoScaleAndPositionDE(logoLayer);
-    // duplicateWebsiteString(x);
-    // duplicatePhoneString(x);
     // randomIntroOutroDE(x);
-    // randomVideoAvira(x,1);
-    // randomVideoAvira(x,2);
-    // randomVideoAvira(x,3);
-    // onlyEnglishAviraAndCity(x);
     onlyEnglishExtras(x);
     stylePrice(x);
 
@@ -226,35 +157,3 @@ function renderIt(x){
     app.project.close(CloseOptions.DO_NOT_SAVE_CHANGES);
 }
 
-
-// 22/01/2021 this section changes the length of the intro comp based on the length of its music layer
-
-function adjustIntroForMusic(x){
-    var mainLayers = x.mainComp.layers;
-    var introComp = mainLayers('0_Intro');
-    var backgroundIntroSong = x.allLayers['0_Intro']['Intro Sound'];
-    var neededTime = backgroundIntroSong.source.duration;
-    var gap = 0.2;
-    // alert(neededTime);
-    var positionIntroBox=introBox.property('Position');
-    var pathMask=introMask.mask(1).property('ADBE Mask Shape');
-
-    var keyNumber=2
-    for (k=1; k <= keyNumber; k++){
-        var posValue= positionIntroBox.keyValue(k);
-        // this is a shape object, the vertices attributes is an array of 4 objects
-        var pathValue= pathMask.keyValue(k);  
-        // alert(posValue[0]);
-        // alert(pathValue.vertices[0]);
-        positionIntroBox.removeKey(k);
-        pathMask.removeKey(k);
-
-        if (k==1){
-            positionIntroBox.setValueAtTime(neededTime-0.5-gap, [posValue[0], posValue[1]]);
-            pathMask.setValueAtTime(neededTime-0.5, pathValue);
-        } else {
-            positionIntroBox.setValueAtTime(neededTime-gap, [posValue[0], posValue[1]]);
-            pathMask.setValueAtTime(neededTime, pathValue);
-        }
-    }
-}
